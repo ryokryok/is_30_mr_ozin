60c88fb9f94c568aec989b81f1757f05541a07cd
mr_ozin <mrozin.dev@gmail.com>
2021-12-21 00:40:59 +0900

    feature ogp

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,11 @@ import { NameLogo, CountDownTimer, Links } from "../components";
 
 const birthName = "Mr_ozin";
 const thirtiesBirthDay = "2021-12-20T00:00:00+09:00";
-const siteTitle = "is_30_Mr_ozin?";
-const siteDesc = "Mr_ozin 三十路タイマー";
+const siteTitle = "Mr_ozin 三十路タイマー";
+const siteDesc = "Mr_ozin が三十路になってからの経過時間をカウントします";
+const siteImage = "/og-image.jpg";
+const siteUrl = "https://is-30-mr-ozin.vercel.app/";
+const siteType = "website";
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +17,13 @@ const Home: NextPage = () => {
         <title>{siteTitle}</title>
         <meta name="description" content={siteDesc} />
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:site_name" content={siteTitle} />
+        <meta property="og:type" content={siteType} />
+        <meta property="og:description" content={siteDesc} />
+        <meta property="og:url" content={siteUrl} />
+        <meta property="og:image" content={siteImage} />
+        <meta name="twitter:card" content={siteImage} />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
